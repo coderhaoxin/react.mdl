d941b08e8f4b5bf75c85ae1184c606cca3774144
haoxin <coderhaoxin@outlook.com>
2016-09-13 17:40:14 +0800

    v1.1.0 Tab - add prop: activeKey

--- example/tab.js
+++ example/tab.js
@@ -5,19 +5,43 @@ import { TabPane, Tabs } from '..'
 
 class TabExample extends Component {
 
+  state = {
+    activeKey: '2'
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      const { activeKey } = this.state
+
+      this.setState({
+        activeKey: String(((activeKey | 0) + 1) % 3)
+      })
+    }, 2000)
+  }
+
   render() {
+    const { activeKey } = this.state
+
     return (
-      <Tabs>
-        <TabPane key='1' tab='one'>
-          hello
-        </TabPane>
-        <TabPane key='2' tab='two'>
-          world
-        </TabPane>
-        <TabPane key='3' tab='three'>
-          go go go
-        </TabPane>
-      </Tabs>
+      <section>
+        <Tabs defaultActiveKey='2'>
+          <TabPane key='1' tab='one'>
+            hello
+          </TabPane>
+          <TabPane key='2' tab='two'>
+            world
+          </TabPane>
+          <TabPane key='3' tab='three'>
+            go go go
+          </TabPane>
+        </Tabs>
+
+        <Tabs activeKey={activeKey}>
+          <TabPane key='0' tab='one'>1</TabPane>
+          <TabPane key='1' tab='two'>2</TabPane>
+          <TabPane key='2' tab='three'>3</TabPane>
+        </Tabs>
+      </section>
     )
   }
 }
